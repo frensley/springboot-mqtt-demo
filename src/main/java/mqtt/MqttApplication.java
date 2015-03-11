ba4bb0e888a6ee28452ef9c017b9340355ce500f
frensley <shane@drillmap.com>
2015-03-10 19:23:33 -0500

    Initial project

---
+++ src/main/java/mqtt/MqttApplication.java
@@ -0,0 +1,12 @@
+package mqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqttApplication.class, args);
+    }
+}
---
+++ src/test/java/mqtt/MqttApplicationTests.java
@@ -0,0 +1,18 @@
+package mqtt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MqttApplication.class)
+@WebAppConfiguration
+public class MqttApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
