80e4624acaf9ea990e3d42047c443446c442dd3c
frensley <shane@drillmap.com>
2015-03-19 17:09:02 -0500

    Remove unecessary injection

--- src/test/java/mqtt/MqttApplicationTests.java
+++ src/test/java/mqtt/MqttApplicationTests.java
@@ -1,20 +1,13 @@
 package mqtt;
 
-import mqtt.repository.TrackRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MqttApplication.class)
 public class MqttApplicationTests {
-
-
-    @SuppressWarnings("SpringJavaAutowiringInspection")
-    @Autowired
-    TrackRepository repository;
 
     @Test
 	public void contextLoads() {
