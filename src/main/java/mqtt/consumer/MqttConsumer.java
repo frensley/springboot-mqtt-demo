be3450dc6bc61d2e36058f74fe15e7838df65fee
frensley <shane@drillmap.com>
2015-03-19 16:12:35 -0500

    Use proximity detector based on points in session

--- src/main/java/mqtt/consumer/MqttConsumer.java
+++ src/main/java/mqtt/consumer/MqttConsumer.java
@@ -97,15 +97,19 @@ public class MqttConsumer {
                     if (trackService.exists(topic,msg)) {
                         log.info("Message exists.");
                     }
-                    //Don't merge another point if it's inside our point tolerance radius because it clutters the map
-                    if (trackService.isWithinDistance(msg, proximityWindow)) {
+
+
+                    Session session = sessionService.findOrCreateSession(topic,sessionWindowSeconds * 1000);
+                    if (session == null) {
+                        log.error("Unable to process message because track session is null.");
+                        return;
+                    }
+                    //Don't merge another point if it's inside our point tolerance radius for this session because it clutters the map
+                    //This will be a problem for circular routes as the final route point will not appear on the map
+                    //perhaps calculate distance and time (or just time)
+                    if (trackService.isWithinDistanceForSession(msg, proximityWindow,session)) {
                         log.info("Message inside radius");
                     } else {
-                        Session session = sessionService.findOrCreateSession(topic,sessionWindowSeconds * 1000);
-                        if (session == null) {
-                            log.error("Unable to process message because track session is null.");
-                            return;
-                        }
                         //Update session date to keep alive
                         session.setDate(System.currentTimeMillis());
                         session = sessionService.save(session);
@@ -117,6 +121,7 @@ public class MqttConsumer {
                     log.error("Saved Exception:", e);
                 }
             } catch (Exception e) {
+                //must catch everything - client will exit if and exception is thrown.
                 log.error("Something bad happened.",e);
             }
         }
